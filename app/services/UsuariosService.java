364f0d87dcce4de60cdbda4c515078f2a2cb782a
Manuel García <mgm143@alu.ua.es>
2016-09-23 02:42:44 +0200

    TIC-6 Funcionalidad de editar usuarios realizada

--- app/services/UsuariosService.java
+++ app/services/UsuariosService.java
@@ -12,22 +12,23 @@ import models.*;
 public class UsuariosService {
 
     public static Usuario grabaUsuario(Usuario usuario) {
+        Logger.debug("Se crea usuario: " + usuario.id);
         return UsuarioDAO.create(usuario);
     }
 
     public static Usuario modificaUsuario(Usuario usuario) {
-      // ToDo
-      return usuario;
+        Logger.debug("Se modifica usuario: " + usuario.id);
+        return UsuarioDAO.update(usuario);
     }
 
     public static Usuario findUsuario(String id) {
       Usuario usuario = UsuarioDAO.find(id);
-      Logger.debug("Se obtiene usuario: " + usuario.id);
+      Logger.debug("Se obtiene usuario: " + id);
       return usuario;
     }
 
     public static boolean deleteUsuario(String id) {
-        // ToDo
+        Logger.debug("Se borra usuario: " + id);
         return false;
     }
 
