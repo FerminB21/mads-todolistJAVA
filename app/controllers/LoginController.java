5640ab0e23fd3a0bb920cf38dc2646f8aa05ff2d
Manuel García <mgm143@alu.ua.es>
2016-10-19 21:16:39 +0200

    TIC-15 Tests sobre UsuarioDao con base de datos en memoria.

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -6,7 +6,9 @@ import javax.inject.*;
 import play.*;
 import play.mvc.*;
 import views.html.*;
+
 import static play.libs.Json.*;
+
 import play.data.Form;
 import play.data.FormFactory;
 import play.db.jpa.*;
@@ -16,84 +18,81 @@ import models.*;
 
 public class LoginController extends Controller {
 
-   @Inject FormFactory formFactory;
+    @Inject
+    FormFactory formFactory;
 
-   // Devuelve un formulario para registrar
-   public Result formularioLogueoRegistro() {
-       return ok(formLogueoRegistro.render(formFactory.form(Usuario.class),"", "logueo"));
-   }
+    // Devuelve un formulario para registrar
+    public Result formularioLogueoRegistro() {
+        return ok(formLogueoRegistro.render(formFactory.form(Usuario.class), "", "logueo"));
+    }
 
-   @Transactional
-   // Verifica si el usuario puede loguearse y devuelve código HTTP
-   // de redirección a la página de portada
-   public Result logueoUsuario() {
+    @Transactional
+    // Verifica si el usuario puede loguearse y devuelve código HTTP
+    // de redirección a la página de portada
+    public Result logueoUsuario() {
 
-     Form<Usuario> usuarioForm = formFactory.form(Usuario.class).bindFromRequest();
-     if (usuarioForm.hasErrors()) {
-         //Le paso un tercer parámetro a la vista para indicar de donde venimos
-         return badRequest(formLogueoRegistro.render(usuarioForm, "Hay errores en el formulario", "logueo"));
-     }
-     Usuario usuario = usuarioForm.get();
-     //Debemos comprobar si ese usuario ya existe en la base de datos
-       Usuario usuarioExistente = UsuariosService.findByLogin(usuario.login);
-     if(usuarioExistente != null){
-       Logger.debug("Existe usuario con ese login");
-       //Comprobación de si el usuario tiene contraseña y nosotros también introducimos una
-       if(usuario.password != null && usuarioExistente.password != null && usuarioExistente.password.equals(usuario.password)){ //Si coinciden contraseña, válido
-         Logger.debug("Logueado correctamente");
-         flash("entraUsuario", "Logueado correctamente.");
-         return redirect(controllers.routes.HomeController.portada());
-       }
-       else if(usuarioExistente.password == null){ //Si es el mismo usuario pero no tiene contraseña (introducido por administrador)
-         Logger.debug("Necesario activar el usuario");
-         return badRequest(formLogueoRegistro.render(usuarioForm, "Usuario no activo. Necesario registrarse para activarlo.", "logueo"));
-       }
-       else{
-         Logger.debug("Error de validación");
-         return badRequest(formLogueoRegistro.render(usuarioForm, "Datos introducidos no coinciden.", "logueo"));
-       }
-     }
-     else{
-        //No existe ningún usuario con ese login
-        Logger.debug("Error de validación");
-        return badRequest(formLogueoRegistro.render(usuarioForm, "Usuario no existe. Regístrate para acceder.", "logueo"));
-     }
-   }
+        Form<Usuario> usuarioForm = formFactory.form(Usuario.class).bindFromRequest();
+        if (usuarioForm.hasErrors()) {
+            //Le paso un tercer parámetro a la vista para indicar de donde venimos
+            return badRequest(formLogueoRegistro.render(usuarioForm, "Hay errores en el formulario", "logueo"));
+        }
+        Usuario usuario = usuarioForm.get();
+        //Debemos comprobar si ese usuario ya existe en la base de datos
+        Usuario usuarioExistente = UsuariosService.findByLogin(usuario.login);
+        if (usuarioExistente != null) {
+            Logger.debug("Existe usuario con ese login");
+            //Comprobación de si el usuario tiene contraseña y nosotros también introducimos una
+            if (usuario.password != null && usuarioExistente.password != null && usuarioExistente.password.equals(usuario.password)) { //Si coinciden contraseña, válido
+                Logger.debug("Logueado correctamente");
+                flash("entraUsuario", "Logueado correctamente.");
+                return redirect(controllers.routes.HomeController.portada());
+            } else if (usuarioExistente.password == null) { //Si es el mismo usuario pero no tiene contraseña (introducido por administrador)
+                Logger.debug("Necesario activar el usuario");
+                return badRequest(formLogueoRegistro.render(usuarioForm, "Usuario no activo. Necesario registrarse para activarlo.", "logueo"));
+            } else {
+                Logger.debug("Error de validación");
+                return badRequest(formLogueoRegistro.render(usuarioForm, "Datos introducidos no coinciden.", "logueo"));
+            }
+        } else {
+            //No existe ningún usuario con ese login
+            Logger.debug("Error de validación");
+            return badRequest(formLogueoRegistro.render(usuarioForm, "Usuario no existe. Regístrate para acceder.", "logueo"));
+        }
+    }
 
-     @Transactional
-     // Registra al usuario si ha insertado bien los datos y devuelve código HTTP
-     // de redirección a la página de portada
-     public Result registroUsuario() {
+    @Transactional
+    // Registra al usuario si ha insertado bien los datos y devuelve código HTTP
+    // de redirección a la página de portada
+    public Result registroUsuario() {
 
-         Form<Usuario> usuarioForm = formFactory.form(Usuario.class).bindFromRequest();
-         String repitePassword = Form.form().bindFromRequest().get("repitePassword");
-         if (usuarioForm.hasErrors()) {
-             //Le paso un tercer parámetro a la vista para indicar de donde venimos
-             return badRequest(formLogueoRegistro.render(usuarioForm, "Hay errores en el formulario", "registro"));
-         }
+        Form<Usuario> usuarioForm = formFactory.form(Usuario.class).bindFromRequest();
+        String repitePassword = Form.form().bindFromRequest().get("repitePassword");
+        if (usuarioForm.hasErrors()) {
+            //Le paso un tercer parámetro a la vista para indicar de donde venimos
+            return badRequest(formLogueoRegistro.render(usuarioForm, "Hay errores en el formulario", "registro"));
+        }
 
-         Usuario usuario = usuarioForm.get();
-         //Debemos comprobar si ese usuario ya existe en la base de datos
-         Usuario usuarioExistente = UsuariosService.findByLogin(usuario.login);
-         if(usuarioExistente != null){
-           //Le paso un tercer parámetro a la vista para indicar de donde venimos
-           return badRequest(formLogueoRegistro.render(usuarioForm, "Ya existe ese login. Inténtalo con otro.", "registro"));
-         }
+        Usuario usuario = usuarioForm.get();
+        //Debemos comprobar si ese usuario ya existe en la base de datos
+        Usuario usuarioExistente = UsuariosService.findByLogin(usuario.login);
+        if (usuarioExistente != null) {
+            //Le paso un tercer parámetro a la vista para indicar de donde venimos
+            return badRequest(formLogueoRegistro.render(usuarioForm, "Ya existe ese login. Inténtalo con otro.", "registro"));
+        }
 
-         //Comprobamos errores relacionados entre los campos del formulario
-         if(usuario.password.equals("") || repitePassword.equals("") || !usuario.password.equals(repitePassword)){
-           return badRequest(formLogueoRegistro.render(usuarioForm, "Ha habido un problema con las contraseñas. Asegúrate de que coincidan y no estén vacías.", "registro"));
-         }
-         else{ //Si funciona
-           //Doy de alta nuevo
-           usuario = UsuariosService.grabaUsuario(usuario);
-           flash("entraUsuario", "Te has dado de alta en TodoList. Bienvenido.");
-           //Si llegamos hasta aquí es que lo hemos registrado de una manera (nuevo) o de otra (modificando datos)
-           Logger.debug("No existe ninguno con ese login, se registra");
-           Logger.debug("Usuario a grabar (registro): " + usuario.toString());
-           Logger.debug("Usuario guardado correctamente: " + usuario.toString());
-           return redirect(controllers.routes.HomeController.portada());
-         }
-      }
+        //Comprobamos errores relacionados entre los campos del formulario
+        if (usuario.password.equals("") || repitePassword.equals("") || !usuario.password.equals(repitePassword)) {
+            return badRequest(formLogueoRegistro.render(usuarioForm, "Ha habido un problema con las contraseñas. Asegúrate de que coincidan y no estén vacías.", "registro"));
+        } else { //Si funciona
+            //Doy de alta nuevo
+            usuario = UsuariosService.grabaUsuario(usuario);
+            flash("entraUsuario", "Te has dado de alta en TodoList. Bienvenido.");
+            //Si llegamos hasta aquí es que lo hemos registrado de una manera (nuevo) o de otra (modificando datos)
+            Logger.debug("No existe ninguno con ese login, se registra");
+            Logger.debug("Usuario a grabar (registro): " + usuario.toString());
+            Logger.debug("Usuario guardado correctamente: " + usuario.toString());
+            return redirect(controllers.routes.HomeController.portada());
+        }
+    }
 
 }
---
+++ test/dao/UsuarioDaoTest.java
@@ -0,0 +1,166 @@
+package dao;
+
+import play.Logger;
+import play.db.Database;
+import play.db.Databases;
+import play.db.jpa.*;
+import org.junit.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import models.*;
+
+import java.util.List;
+
+public class UsuarioDaoTest {
+
+    Database db;
+    JPAApi jpa;
+
+    @Before
+    public void initDatabase() {
+        db = Databases.inMemoryWith("jndiName", "DefaultDS");
+        // Necesario para inicializar el nombre JNDI de la BD
+        db.getConnection();
+        // Se activa la compatibilidad MySQL en la BD H2
+        db.withConnection(connection -> {
+            connection.createStatement().execute("SET MODE MySQL;");
+        });
+        jpa = JPA.createFor("memoryPersistenceUnit");
+    }
+
+    @After
+    public void shutdownDatabase() {
+        //Después de cada test borra la tabla
+        db.withConnection(connection -> {
+            connection.createStatement().execute("DROP TABLE Usuario;");
+        });
+        jpa.shutdown();
+        db.shutdown();
+    }
+
+    /**
+     * Crea un usuario nuevo (pepe) y comprobamos si coincide con lo que esperamos.
+     * Métodos que se prueban:
+     *  - create
+     *  - find
+     */
+    @Test
+    public void creaBuscaUsuario() {
+
+        //Creamos el usuario
+        Integer id = jpa.withTransaction(() -> {
+            Usuario nuevo = new Usuario("pepe", "pepe");
+            nuevo = UsuarioDAO.create(nuevo);
+            return nuevo.id;
+        });
+
+        //Comprobamos que existe
+        jpa.withTransaction(() -> {
+            Usuario usuario = UsuarioDAO.find(id);
+            assertThat(usuario.login, equalTo("pepe"));
+        });
+    }
+
+    /**
+     * Busca el usuario por login
+     * Métodos que se prueban:
+     *  - findByLogin
+     */
+    @Test
+    public void buscaUsuarioLogin() {
+
+        //Comprobamos que no existe
+        jpa.withTransaction(() -> {
+            Usuario usuario = UsuarioDAO.findByLogin("pepe");
+            assertNull(usuario);
+        });
+    }
+
+    /**
+     * Comprobamos los siguientes métodos para cambiar el login de un usuario:
+     *  - create
+     *  - find
+     *  - update
+     */
+    @Test
+    public void modificaUsuario(){
+
+        //Creamos el usuario
+        int id = jpa.withTransaction(() -> {
+            Usuario nuevo = new Usuario("pepe", "pepe");
+            Usuario usuario = UsuarioDAO.create(nuevo);
+            return nuevo.id;
+        });
+
+        //Modificamos usuario
+        jpa.withTransaction(() -> {
+            Usuario busqueda = UsuarioDAO.find(id);
+            busqueda.login = "pruebaUpdate"; //no hace falta llamar al update
+        });
+
+        //Comprobamos que se ha cambiado
+        jpa.withTransaction(() -> {
+            Usuario usuarioModificado = UsuarioDAO.find(id); //volvemos a buscar
+            assertThat(usuarioModificado.login, equalTo("pruebaUpdate"));
+        });
+    }
+
+    /**
+     * Comprobamos los siguientes métodos para cambiar el login de un usuario:
+     *  - create
+     *  - find
+     *  - delete
+     */
+    @Test
+    public void borraUsuario(){
+
+        //Creamos el usuario
+        int id = jpa.withTransaction(() -> {
+            Usuario nuevo = new Usuario("pepe", "pepe");
+            Usuario usuario = UsuarioDAO.create(nuevo);
+            return nuevo.id;
+        });
+
+        //Eliminamos usuario
+        jpa.withTransaction(() -> {
+            UsuarioDAO.delete(id);
+        });
+
+        //Comprobamos que no existe
+        jpa.withTransaction(() -> {
+            Usuario busqueda = UsuarioDAO.find(id);
+            assertNull(busqueda);
+        });
+    }
+
+    /**
+     * Comprobamos los siguientes métodos para devolver la cantidad de usuarios creados:
+     *  - create
+     *  - find
+     *  - delete
+     */
+    @Test
+    public void cantidadUsuarios(){
+
+        //Creamos el usuario 1
+        int id = jpa.withTransaction(() -> {
+            Usuario nuevo = new Usuario("pepe", "pepe");
+            Usuario usuario = UsuarioDAO.create(nuevo);
+            return nuevo.id;
+        });
+
+        //Creamos el usuario 2
+        int id2 = jpa.withTransaction(() -> {
+            Usuario nuevo = new Usuario("jorge", "jorge");
+            Usuario usuario = UsuarioDAO.create(nuevo);
+            return nuevo.id;
+        });
+
+        //Comprobamos cantidad usuarios
+        jpa.withTransaction(() -> {
+            List<Usuario> usuarios = UsuarioDAO.findAll();
+            assertEquals(2, usuarios.size()); //primero esperado, después real (al contrario que assertThat)
+        });
+    }
+}
