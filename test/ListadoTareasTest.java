546a58dfbd22f6d3f995751bd9b8309dfa91c0e9
Manuel García <mgm143@alu.ua.es>
2016-10-22 14:05:26 +0200

    TIC-19 Listado de tareas utilizando el service

--- test/ListadoTareasTest.java
+++ test/ListadoTareasTest.java
@@ -11,8 +11,10 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import models.*;
+import services.*;
 
 public class ListadoTareasTest {
 
@@ -92,6 +94,24 @@ public class ListadoTareasTest {
         jpa.withTransaction(() -> {
             Usuario usuario = UsuarioDAO.find(1);
             assertEquals(usuario.tareas.size(), 3);
+        });
+    }
+
+    @Test
+    public void listadoTareasService() {
+        jpa.withTransaction(() -> {
+            List<Tarea> tareas = TareasService.listaTareasUsuario(1);
+            assertEquals(tareas.size(), 3);
+
+            // Comprobamos que las tareas se devuelven ordenadas por id
+
+            Tarea anterior = null;
+            for (Tarea t : tareas) {
+                if (anterior != null) {
+                    assertTrue(anterior.id < t.id);
+                    anterior = t;
+                }
+            }
         });
     }
 }
