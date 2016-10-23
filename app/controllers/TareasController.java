4b6fa6e8d9e55b5ca77ebf30997ce71b8a5dfa97
Manuel García <mgm143@alu.ua.es>
2016-10-23 15:14:51 +0200

    TIC-21 Controller y vista de la creación de una tarea asociada a un usuario

--- app/controllers/TareasController.java
+++ app/controllers/TareasController.java
@@ -16,6 +16,8 @@ import java.util.List;
 
 public class TareasController extends Controller {
 
+    @Inject FormFactory formFactory;
+
     @Transactional(readOnly = true)
     public Result listaTareas(Integer idUsuario) {
         String aviso = flash("aviso");
@@ -29,5 +31,29 @@ public class TareasController extends Controller {
             List<Tarea> tareas = TareasService.listaTareasUsuario(idUsuario);
             return ok(listaTareas.render(tareas, usuario, aviso, error));
         }
+    }
+
+    // Devuelve un formulario para crear un nuevo usuario
+    public Result formularioNuevaTarea(Integer idUsuario) {
+        return ok(formCreacionTarea.render(formFactory.form(Tarea.class), idUsuario, ""));
+    }
+
+    @Transactional
+    // Añade una nueva tarea en la BD y devuelve código HTTP
+    // de redirección a la página de listado de usuarios
+    //PARA EJECUTAR ESTE MÉTODO HA SIDO NECESARIO REALIZAR: activator run
+    //Daba error como si no detectara que el campo descripcion de Tarea no existiera
+    public Result grabaNuevaTarea(Integer idUsuario) {
+
+        Form<Tarea> tareaForm = formFactory.form(Tarea.class).bindFromRequest();
+        if (tareaForm.hasErrors()) {
+            return badRequest(formCreacionTarea.render(tareaForm, idUsuario, "Hay errores en el formulario"));
+        }
+        Tarea tarea = tareaForm.get();
+        Logger.debug("Tarea a grabar: " + tarea.toString());
+        tarea = TareasService.crearTareaUsuario(tarea.descripcion, idUsuario);
+        flash("gestionaTarea", "La tarea se ha grabado correctamente");
+        Logger.debug("Usuario guardado correctamente: " + tarea.toString());
+        return redirect(routes.TareasController.listaTareas(idUsuario));
     }
 }
