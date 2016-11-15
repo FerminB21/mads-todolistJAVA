5888e7d1376d522ea65e66bb40fbf02fd94c713d
Manuel García <mgm143@alu.ua.es>
2016-11-15 18:58:11 +0100

    Vista creada de la página Acerca De y con mi nombre de usuario Manuel García Menárguez en ella

---
+++ app/controllers/AcercaDeController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import play.mvc.*;
+
+import views.html.*;
+
+
+
+
+/**
+ * Created by mads on 15/11/16.
+ */
+public class AcercaDeController extends Controller {
+
+    // Devuelve la página acerca-de
+    public Result acercaDe() {
+        return ok(acercaDe.render());
+    }
+}
